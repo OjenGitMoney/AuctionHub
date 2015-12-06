b771a722439045b795df56cd92875529de074d68
ahmed1993 <ahmedcollege93@gmail.com>
2015-12-05 19:37:54 -0800

    sql files

--- cs174_Project.bat
+++ cs174_Project.bat
@@ -7,10 +7,9 @@ DB2 GRANT SECADM ON DATABASE TO USER DB2ADMIN
 DB2 -td"^" -f scripts/create_tables.sql
 
 
-DB2 -td"^" -f scripts/insert_items.sql
-DB2 -td"^" -f scripts/insert_users.sql
+DB2 -td"^" -f scripts/insert_all_items.sql
+DB2 -td"^" -f scripts/insert_all_users.sql
 rem DB2 -td"^" -f scripts/insert_bids.sql
 
-DB2 -td"^" -f scripts/insert_items2.sql
 
 
