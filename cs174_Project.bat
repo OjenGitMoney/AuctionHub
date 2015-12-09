4833f1274a59d973272fba98fb07f7a6477fdaaa
OjenGitMoney <ojen.goshtasb@gmail.com>
2015-12-08 22:37:26 -0800

    Script Fixed
    Script Fixed

--- cs174_Project.bat
+++ cs174_Project.bat
@@ -3,13 +3,14 @@ DB2 CREATE DATABASE Auction
 
 DB2 CONNECT TO Auction
 DB2 GRANT SECADM ON DATABASE TO USER DB2ADMIN
+DB2 GRANT DBADM ON DATABASE TO USER DB2ADMIN
 
 DB2 -td"^" -f scripts/create_tables.sql
 
 DB2 -td"^" -f scripts/insert_all_items.sql
 DB2 -td"^" -f scripts/insert_all_users.sql
 
-start index.php
+rem start index.php
 
 
 
